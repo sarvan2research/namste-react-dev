5f05216e3418922146cfd28f05207ffdfadc6cdb
saravanakumar ravichandran <saravanakumar.ravichandran@oracle.com>
2024-10-30 06:54:19 +0530

    Multiple child divs

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 /*
 <div id=parent>
-<div id=child>
-<h1>This H1 tag via div's</h1>
-<h2>This H1 tag via div's</h2>
-</div>
+  <div id=child>
+    <h1>This H1 tag via div's</h1>
+    <h2>This H1 tag via div's</h2>
+  </div>
+  <div id=child>
+    <h1>This H1 tag via div's</h1>
+    <h2>This H1 tag via div's</h2>
+  </div>
 </div>
 */
 
 const parent=React.createElement(
   "div",
   {id:"parent"},
-  React.createElement(
+  [React.createElement(
     "div",
     {id:"child"},
     [React.createElement("h1",{},"This H1 tag via parent/child div's"),
     React.createElement("h2",{},"This H2 tag via parent/child div's")]
-  )
+  ),
+  React.createElement(
+    "div",
+    {id:"child2"},
+    [React.createElement("h1",{},"This H1 tag via parent/child div's"),
+    React.createElement("h2",{},"This H2 tag via parent/child div's")])]
 );
 
 console.log(parent);
