656c39d7146f7e00afe7d16409730250bd78ae1c
saravanakumar ravichandran <saravanakumar.ravichandran@oracle.com>
2024-10-30 06:46:50 +0530

    Adding multiple div elements using react

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const heading = React.createElement("h1",{id:"heading"},"This is from react Namaste Dev!!"); // Returns JS object
+/*
+<div id=parent>
+<div id=child>
+<h1>This H1 tag via div's</h1>
+</div>
+</div>
+*/
 
-console.log(heading);
+const parent=React.createElement(
+  "div",
+  {id:"parent"},
+  React.createElement(
+    "div",
+    {id:"child"},
+    React.createElement("h1",{},"This H1 tag via parent/child div's")
+  )
+);
+
+console.log(parent);
+
 const rootDom=ReactDOM.createRoot(document.getElementById("root")); // Dom used to update element on screen.
-rootDom.render(heading); // return js object to html element
+rootDom.render(parent); // return js object to html element
