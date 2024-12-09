35af23ae26e85d8900d3911c7c33448de7572c4a
saravanakumar ravichandran <saravanakumar.ravichandran@oracle.com>
2024-12-09 20:07:46 +0530

    Adding Login button showcasing useStateWorking.

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const HeaderContainer=() =>
 {
-  (
+  const[buttonName,setButtonName]=useState("Login");
+
+  return (
   <div className="header">
     <div className="logo-container">
       <img className="logo"src={LOGO_URL}></img>
@@ -13,7 +16,12 @@ const HeaderContainer=() =>
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
-          <button className="login-btn"></button>
+          <button className="login-btn"
+          onClick={()=>{
+            buttonName==="Login"?
+            setButtonName("Logout"):
+            setButtonName("Login")
+          }}>{buttonName}</button>
         </ul>
     </div>
   </div>
