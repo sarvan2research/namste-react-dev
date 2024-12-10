7da0fa1a8bb9f2951c083df24858160289f11ad1
saravanakumar ravichandran <saravanakumar.ravichandran@oracle.com>
2024-12-10 11:02:09 +0530

    UseEffect understanding commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const HeaderContainer=() =>
 {
   const[buttonName,setButtonName]=useState("Login");
+  console.log("Header Called")
+  // If no dependency its called on each renders.
+  // If Empty Array its called only once when first time screen renders
+  // If any state variable given then only called when its getting changed.
+  useEffect(()=>{
+    console.log("Header called inside UseEffect")
+  },[buttonName])
 
   return (
   <div className="header">
