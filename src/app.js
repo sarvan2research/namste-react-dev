7f7aa4b218a62be40a6f24fa7dd7e0db01478c69
saravanakumar ravichandran <saravanakumar.ravichandran@oracle.com>
2024-12-14 22:26:13 +0530

    Lazy loading or chunking

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import HeaderContainer from "./component/Header";
 import BodyContainer from "./component/Body";
@@ -7,6 +7,7 @@ import About from "./component/About";
 import Contact from "./component/Contact";
 import Error from "./component/Error";
 import RestaurantDetails from "./component/RestaurantDetails";
+import Shimmer from "./component/Shimmer";
 
 /*
 AppLayout
@@ -24,41 +25,55 @@ AppLayout
     - Address
 */
 
-const AppLayout=()=>(
+// to make app loadtiming faster
+// use of chunking,lazy loading, ondemand loading
+// using lazy feature from react make it easy(use lazy import and add suspense so react can pull it behind screen until that fallback screen (shimmerUI) shown to user)
+//
+
+const Grocery = lazy(() => import("./component/Grocery"));
+
+const AppLayout = () => (
   <div className="app">
-    <HeaderContainer/>
-    <Outlet/>
+    <HeaderContainer />
+    <Outlet />
   </div>
-) 
+);
 //Step1: Creating routes
-paths=[
+paths = [
   {
-    path:"/",
-    element:<AppLayout/>,
-    children:[
+    path: "/",
+    element: <AppLayout />,
+    children: [
       {
-        path:"/",
-        element:<BodyContainer />,
+        path: "/",
+        element: <BodyContainer />,
       },
       {
-        path:"/about",
-        element:<About />,
+        path: "/about",
+        element: <About />,
       },
       {
-        path:"/contact",
-        element:<Contact />,
+        path: "/contact",
+        element: <Contact />,
       },
       {
-        path:"/restaurants/:resID",
-        element:<RestaurantDetails />,
+        path: "/restaurants/:resID",
+        element: <RestaurantDetails />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <Grocery />
+          </Suspense>
+        ),
       },
     ],
-    errorElement:<Error />
-  }
-  
-]
-const routes=createBrowserRouter(paths)
-const rootDom=ReactDOM.createRoot(document.getElementById("root")); // Dom used to update element on screen.
+    errorElement: <Error />,
+  },
+];
+const routes = createBrowserRouter(paths);
+const rootDom = ReactDOM.createRoot(document.getElementById("root")); // Dom used to update element on screen.
 //rootDom.render(<AppLayout />); // return js object to html elementxxs
 //Step2:
-rootDom.render(<RouterProvider router={routes}/>)
+rootDom.render(<RouterProvider router={routes} />);
---
+++ src/component/Grocery.js
@@ -0,0 +1,5 @@
+const Grocery = () => {
+  return <h1>Its heavy Grocery page holds too many components</h1>;
+};
+
+export default Grocery;
